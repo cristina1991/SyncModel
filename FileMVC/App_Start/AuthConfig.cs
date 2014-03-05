d303f597e33f841e93e70b977e10bbf25dc4b565
cristina1991 <cristina.pereteatco@gmail.com>
2014-03-04 17:51:45 -0800

    oauth

--- FileMVC/App_Start/AuthConfig.cs
+++ FileMVC/App_Start/AuthConfig.cs
@@ -11,18 +11,18 @@ namespace FileMVC
     {
         public static void RegisterAuth()
         {
-            OAuthWebSecurity.RegisterMicrosoftClient(
-                clientId: "000000004C10C01A",
-                clientSecret: "NQRkZAtTHwjbsIeESF6exh02XPwshsrz");
+        //    OAuthWebSecurity.RegisterMicrosoftClient(
+        //        clientId: "000000004C10C01A",
+        //        clientSecret: "NQRkZAtTHwjbsIeESF6exh02XPwshsrz");
 
-            OAuthWebSecurity.RegisterTwitterClient(
-               consumerKey: "BH2aXJW3LXxdaYpBdGzQ",
-               consumerSecret: "q5fZyWwOEW9mIXVAxRd1VygY0pqUR9HiaA7cQIKZQ");
-            OAuthWebSecurity.RegisterFacebookClient(
-               appId: "187654464761762",
-               appSecret: "62cfd93111645f7a956a0625adc3d2c5");
+        //    OAuthWebSecurity.RegisterTwitterClient(
+        //       consumerKey: "BH2aXJW3LXxdaYpBdGzQ",
+        //       consumerSecret: "q5fZyWwOEW9mIXVAxRd1VygY0pqUR9HiaA7cQIKZQ");
+        //    OAuthWebSecurity.RegisterFacebookClient(
+        //       appId: "187654464761762",
+        //       appSecret: "62cfd93111645f7a956a0625adc3d2c5");
 
-            OAuthWebSecurity.RegisterGoogleClient();
+            //OAuthWebSecurity.RegisterGoogleClient();
         }
     }
 }
