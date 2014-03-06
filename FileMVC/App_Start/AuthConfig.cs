de5faf39418b5aed3c0b76646d4c733d272aa867
cristina1991 <cristina.pereteatco@gmail.com>
2014-03-06 04:34:04 -0800

    removed twitter and facebook login

--- FileMVC/App_Start/AuthConfig.cs
+++ FileMVC/App_Start/AuthConfig.cs
@@ -15,12 +15,12 @@ namespace FileMVC
                 clientId: "000000004C10C01A",
                 clientSecret: "NQRkZAtTHwjbsIeESF6exh02XPwshsrz");
 
-            OAuthWebSecurity.RegisterTwitterClient(
-               consumerKey: "BH2aXJW3LXxdaYpBdGzQ",
-               consumerSecret: "q5fZyWwOEW9mIXVAxRd1VygY0pqUR9HiaA7cQIKZQ");
-            OAuthWebSecurity.RegisterFacebookClient(
-               appId: "187654464761762",
-               appSecret: "62cfd93111645f7a956a0625adc3d2c5");
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //   consumerKey: "BH2aXJW3LXxdaYpBdGzQ",
+            //   consumerSecret: "q5fZyWwOEW9mIXVAxRd1VygY0pqUR9HiaA7cQIKZQ");
+            //OAuthWebSecurity.RegisterFacebookClient(
+            //   appId: "187654464761762",
+            //   appSecret: "62cfd93111645f7a956a0625adc3d2c5");
 
             OAuthWebSecurity.RegisterGoogleClient();
         }
