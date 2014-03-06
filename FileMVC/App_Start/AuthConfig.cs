2e1ec8aa15c197189f209f197eb86d396cd1b464
cristina1991 <cristina.pereteatco@gmail.com>
2014-03-06 06:41:36 -0800

    xxxx

--- FileMVC/App_Start/AuthConfig.cs
+++ FileMVC/App_Start/AuthConfig.cs
@@ -11,13 +11,13 @@ namespace FileMVC
     {
         public static void RegisterAuth()
         {
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "000000004C10C01A",
-            //    clientSecret: "NQRkZAtTHwjbsIeESF6exh02XPwshsrz");
+            OAuthWebSecurity.RegisterMicrosoftClient(
+                clientId: "000000004C10C01A",
+                clientSecret: "NQRkZAtTHwjbsIeESF6exh02XPwshsrz");
 
-            //OAuthWebSecurity.RegisterTwitterClient(
-            //   consumerKey: "BH2aXJW3LXxdaYpBdGzQ",
-            //   consumerSecret: "q5fZyWwOEW9mIXVAxRd1VygY0pqUR9HiaA7cQIKZQ");
+            OAuthWebSecurity.RegisterTwitterClient(
+               consumerKey: "BH2aXJW3LXxdaYpBdGzQ",
+               consumerSecret: "q5fZyWwOEW9mIXVAxRd1VygY0pqUR9HiaA7cQIKZQ");
             OAuthWebSecurity.RegisterFacebookClient(
                appId: "187654464761762",
                appSecret: "62cfd93111645f7a956a0625adc3d2c5");
