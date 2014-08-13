9739399777fbb285c3109b33e7c2a3ee48bd5207
cristina1991 <cristina.pereteatco@gmail.com>
2014-08-13 18:10:20 +0300

    new commit dupa stricat

---
+++ SyncModel.Tests/Controllers/FilesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SyncModel.Tests.Controllers
+{
+    [TestClass]
+   public class FilesController
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            FilesController controller = new FilesController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual(null, result.ViewBag.Message);
+        }
+    }
+}
